1af1b0f0942e40fdc7dcf95c3b0d9505743a82a3
Eduardo Machado <up202105337@g.uporto.pt>
2022-10-18 18:15:03 +0100

    coco xixi

---
+++ UC.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Eduardo Afonso Soares Ferreira Machado on 18/10/2022.
+//
+
+#include "UC.h"
---
+++ UC.h
@@ -0,0 +1,14 @@
+//
+// Created by Eduardo Afonso Soares Ferreira Machado on 18/10/2022.
+//
+
+#ifndef UNTITLED_UC_H
+#define UNTITLED_UC_H
+
+
+class UC {
+
+};
+
+
+#endif //UNTITLED_UC_H
