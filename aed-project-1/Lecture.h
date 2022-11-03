b9760d003a89e9ce6e824bcde72993c3f9fe1a31
Joao Guedes <up202108711@up.pt>
2022-11-03 17:27:30 +0000

    Lecture Doxy

--- aed-project-1/Lecture.h
+++ aed-project-1/Lecture.h
@@ -19,13 +19,49 @@ private:
     string type_;
 
 public:
+    /**
+    * Constructor.
+    * @brief Complexidade : O(1)
+    */
     Lecture();
+    /**
+    * @brief Complexidade : O(n)
+    * @param 1. uc_code
+    * @param 2. weekday
+    * @param 3. start_hour
+    * @param 4. duration
+    * @param 5. type
+    */
     Lecture(string uc_code, string weekday, float start_hour, float duration, string type);
+    /**
+     * @brief Complexidade : O(n)
+     * @return
+     */
     string getUcCode();
+    /**
+     * @brief Complexidade : O(n)
+     * @return
+     */
     string getWeekday();
+    /**
+    * @brief Complexidade : O(n)
+    * @return
+    */
     float getStartHour();
+    /**
+     * @brief Complexidade : O(n)
+     * @return
+     */
     float getDuration();
+    /**
+     * @brief Complexidade : O(n)
+     * @return
+     */
     string getType();
+    /**
+     * @brief Complexidade : O(n)
+     * @param 1. l
+     */
     Lecture(Lecture const &l); //copy constructor
 
 };
