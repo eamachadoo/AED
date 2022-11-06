fd50b0fe6d26d75339095aec251367cd50d8cdb4
Eduardo Machado <up202105337@g.uporto.pt>
2022-11-06 18:02:23 +0000

    menu

--- aed-project-1/GestorHorarios.cpp
+++ aed-project-1/GestorHorarios.cpp
@@ -139,37 +139,49 @@ void GestorHorarios::readStudents() {
     }
 }
 
+void
+
 void GestorHorarios::Menu() {
     bool loop = true;
     while (loop) {
         cout << "-------------------------" << endl;
         cout << " GESTOR DE HORARIOS LEIC" << endl;
         cout << "-------------------------" << endl;
-        cout << endl;
         cout << "\tMenu Principal\n";
         cout << "(1) Ocupação de turmas/UC/ano" << endl;
         cout << "(2) Horário de aluno" << endl;
         cout << "(3) Estudantes em determinada turma/UC/ano" << endl;
         cout << "(0) Sair";
+        cout << endl;
         cout << "->";
 
         vector<int> inputs = {0, 1, 2, 3};
-        char input;
+        int input;
         cin >> input;
 
         switch (input) {
-            case '1':
-                string input2;
+            case 1:
+                cout << "(1) Turma" << endl;
+                cout << "(2) UC" << endl;
+                cout << "(3) Ano" << endl;
+                int input2;
                 cin >> input2;
+                if (input2==1){
 
+                }
+                if (input2==2){
+
+                }
+                if(input2 == 3)
+                    loop = false;
                 break;
-            case '2':
+            case 2:
                 cout << "coco" << endl;
                 break;
-            case '3':
+            case 3:
                 cout << "pp" << endl;
                 break;
-            case '0':
+            case 0:
                 loop = false;
                 break;
             default:
